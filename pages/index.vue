a06b9e1f2087f1975ad3fca1568d336f3aee4e78
Allan-Sanchez <asanchezrixtun@gmail.com>
2020-06-10 14:17:49 -0600

    app pwa store app init

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+   <h1 class="text-red-500">Principal</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  // components: {
+  //   Logo
+  // }
+}
+</script>
+
+<style>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+  @apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+</style>
